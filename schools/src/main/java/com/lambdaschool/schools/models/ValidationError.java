80100025efa93e0618c1e1fab23ceb5d686066ab
JDMTias <matiasiturbide@gmail.com>
2020-08-18 16:14:12 -0600

    appprop,errordetail,customeerredetails,and helperfunctions done

---
+++ schools/src/main/java/com/lambdaschool/schools/models/ValidationError.java
@@ -0,0 +1,30 @@
+package com.lambdaschool.schools.models;
+
+public class ValidationError {
+    private String Code;
+    private String message;
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "ValidationError{" +
+                "Code='" + Code + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
